1df971a116699b7b269d287b66d817b91e4af8ff
Janis Sprenger <Janis.Sprenger@dfki.de>
2022-03-17 15:49:21 +0100

    Added Adapter scripts

---
+++ Runtime/Adapter/UnityLogger.cs
@@ -0,0 +1,62 @@
+﻿// SPDX-License-Identifier: MIT
+// The content of this file has been developed in the context of the MOSIM research project.
+// Original author(s): Felix Gaisbauer, Andreas Kaiser
+
+using UnityEngine;
+
+namespace MMIAdapterUnity
+{
+    /// <summary>
+    /// Implementation of a logger which outputs the text on the unity console
+    /// </summary>
+    public class UnityLogger : MMICSharp.Logger
+    {
+
+        /// <summary>
+        /// Flag which specifies whether a server build is utilized
+        /// </summary>
+        private bool isServerBuild = false;
+
+        /// <summary>
+        /// Basic constructor
+        /// </summary>
+        /// <param name="isServerBuild"></param>
+        public UnityLogger(bool isServerBuild = false)
+        {
+            this.isServerBuild = isServerBuild;
+        }
+
+		protected override void CreateDebugLog(string text)
+        {
+            if (isServerBuild)
+                base.CreateDebugLog(text);
+            else
+                Debug.Log(text);
+            
+        }
+
+        protected override void CreateErrorLog(string text)
+        {
+            if (isServerBuild)
+                base.CreateErrorLog(text);
+            else
+                Debug.LogError(text);
+        }
+
+        protected override void CreateInfoLog(string text)
+        {
+            if (isServerBuild)
+                base.CreateInfoLog(text);
+            else
+                Debug.Log(text);
+        }
+
+		protected override void CreateWarningLog(string text)
+		{
+			if (isServerBuild)
+				base.CreateWarningLog(text);
+			else
+				Debug.LogWarning(text);
+		}
+	}
+}
