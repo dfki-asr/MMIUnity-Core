5d5ac2e4232a4e0af1c263bff7635975c17fa69a
Janis Sprenger <Janis.Sprenger@dfki.de>
2022-03-17 15:20:49 +0100

    Moved MMIUnity code here and setup MMICSharp-Base linkage

---
+++ Runtime/MMIUnity/Retargeting/VisualizationJoint.cs
@@ -0,0 +1,246 @@
+// SPDX-License-Identifier: MIT
+// The content of this file has been developed in the context of the MOSIM research project.
+// Original author(s): Janis Sprenger
+
+
+using System.Collections.Generic;
+using UnityEngine;
+using MMICSharp.Common;
+using MMIStandard;
+
+namespace MMIUnity.Retargeting
+{
+
+    public class ISVisualizationJoint
+    {
+
+        // Hard reference to human bones if possible. In case of the spine, this will be null. 
+        //private HumanBodyBones referenceBone { get; }
+        public Transform reference { get; set; }
+
+
+        // Base rotations of human bones with respect to the reference bone. 
+        private Quaternion humanBoneBaseRotation { get; set; } = Quaternion.identity;
+        private Quaternion inverseBaseRotation = Quaternion.identity;
+
+        // Game object for visualization. 
+        private GameObject gameJoint { get; set; }
+
+        //private Animator anim;
+
+        private List<ISVisualizationJoint> children = new List<ISVisualizationJoint>();
+        public ISVisualizationJoint parent { get; private set; }
+        private MMICSharp.Common.RJoint j;
+
+
+        public ISVisualizationJoint(MMICSharp.Common.RJoint jBase, Transform RootBone, Dictionary<MJointType, string> bonenameMap)
+        {
+            if (bonenameMap.ContainsKey(jBase.GetMJoint().Type))
+            {
+                string name = bonenameMap[jBase.GetMJoint().Type];
+                this.reference = RootBone.GetChildRecursiveByName(name);
+            }
+            else
+            {
+                this.reference = null;
+            }
+            this.j = jBase;
+
+            for (int i = 0; i < jBase.children.Count; i++)
+            {
+                MMICSharp.Common.RJoint child = (RJoint)jBase.children[i];
+                ISVisualizationJoint retjoint = new ISVisualizationJoint(child, RootBone, bonenameMap);
+                retjoint.parent = this;
+                this.children.Add(retjoint);
+            }
+        }
+
+        public void Destroy()
+        {
+            GameObject.Destroy(this.gameJoint);
+        }
+
+
+        public List<double> GetManualPostureValues()
+        {
+            List<double> vals = new List<double>();
+            this.GetManualPostureValues(vals);
+            return vals;
+        }
+
+        private void GetManualPostureValues(List<double> values)
+        {
+            Quaternion q = (this.inverseBaseRotation * this.gameJoint.transform.localRotation);
+            Vector3 pos = this.GetPositionAnimation();
+
+            for (int i = 0; i < this.j.GetChannels().Count; i++)
+            {
+                switch (this.j.GetChannels()[i])
+                {
+                    case MChannel.WRotation:
+                        values.Add(q.w);
+                        break;
+                    case MChannel.XRotation:
+                        values.Add(q.x);
+                        break;
+                    case MChannel.YRotation:
+                        values.Add(q.y);
+                        break;
+                    case MChannel.ZRotation:
+                        values.Add(q.z);
+                        break;
+                    case MChannel.XOffset:
+                        values.Add(pos.x);
+                        break;
+                    case MChannel.YOffset:
+                        values.Add(pos.y);
+                        break;
+                    case MChannel.ZOffset:
+                        values.Add(pos.z);
+                        break;
+                }
+            }
+
+            foreach (var child in this.children)
+            {
+                child.GetManualPostureValues(values);
+            }
+
+        }
+
+
+        /// <summary>
+        /// Updates the animation data based on the current intermediate skeleton configuration. This function has to be used in case of manual animation of the intermediate skeleton. 
+        /// </summary>
+        public void ApplyPostureValues()
+        {
+            this.gameJoint.transform.rotation = this.j.GetGlobalRotation().ToQuaternion();
+
+            if (this.j.GetChannels().Contains(MChannel.XOffset))
+            {
+                this.gameJoint.transform.position = this.j.GetGlobalPosition().ToVector3();
+            }
+
+            foreach (ISVisualizationJoint child in this.children)
+            {
+                child.ApplyPostureValues();
+            }
+        }
+
+        public Vector3 GetPositionAnimation()
+        {
+            Vector3 dir = this.gameJoint.transform.localPosition;
+            dir -= this.j.GetOffsetPositions().ToVector3();
+            dir = Quaternion.Inverse(this.j.GetOffsetRotation().ToQuaternion()) * dir;
+
+            return dir;
+        }
+
+        public Matrix4x4 GetGlobalMatrix()
+        {
+            var t = j.GetOffsetPositions().ToVector3();
+            var q = j.GetOffsetRotation().ToQuaternion();
+            var m = Matrix4x4.TRS(t, q, Vector3.one);
+
+            if (parent != null)
+            {
+                var parentM = parent.GetGlobalMatrix();
+                m = parentM * m;
+            }
+
+            return m;
+        }
+
+
+        /// <summary>
+        /// Creates the Game Objects for the skeleton joints. The Game Objects are not only for visualization but provide access to Unity3D transformation classes as well. 
+        /// </summary>
+        /// <param name="jointPrefab"></param>
+        public void CreateGameObjSkel(GameObject jointPrefab)
+        {
+            // Instantiate new object
+            this.gameJoint = Object.Instantiate<GameObject>(jointPrefab);
+            this.gameJoint.name = this.j.GetMJoint().ID;
+            Vector3 parentPosition = new Vector3(0, 0, 0);
+
+            // Set parent
+            if (this.parent != null)
+            {
+                gameJoint.transform.parent = this.parent.gameJoint.transform;
+            }
+            else
+            {
+            }
+
+            // Set transformations
+            gameJoint.transform.localScale = new Vector3(1, 1, 1);
+            gameJoint.transform.localRotation = new Quaternion((float)this.j.GetOffsetRotation().X, (float)this.j.GetOffsetRotation().Y, (float)this.j.GetOffsetRotation().Z, (float)this.j.GetOffsetRotation().W);
+            gameJoint.transform.localPosition = new Vector3((float)this.j.GetOffsetPositions().X, (float)this.j.GetOffsetPositions().Y, (float)this.j.GetOffsetPositions().Z);
+
+
+
+            // compute inverse rotations
+            this.inverseBaseRotation = Quaternion.Inverse(gameJoint.transform.localRotation);
+
+            // This is a hack. Changing the scale above messes up the child joints. Hence, additional Scaled joints as dummy objects are utilized. 
+            GameObject jointScaled = Object.Instantiate<GameObject>(jointPrefab);
+            jointScaled.name = this.gameJoint.name;
+            jointScaled.transform.parent = gameJoint.transform;
+            jointScaled.transform.localPosition = Vector3.zero;
+            jointScaled.transform.localRotation = Quaternion.identity;
+            float visWidth = 20;
+            float visLength = 100 * (float)this.j.GetBoneLength();
+            string jointID = this.j.GetMJoint().ID;
+            if (jointID.Contains("Proximal") || jointID.Contains("Meta") || jointID.Contains("Distal") || jointID.Contains("Mid") || jointID.Contains("Carpal"))
+            {
+                visWidth = 5;
+            }
+            if (jointID.Contains("Tip"))
+            {
+                visWidth = 2;
+                visLength = 0.001f;
+            }
+            jointScaled.transform.localScale = new Vector3(visWidth, visLength, visWidth);
+
+            // Recurse
+            foreach (ISVisualizationJoint child in this.children)
+            {
+                child.CreateGameObjSkel(jointPrefab);
+            }
+        }
+
+        public MAvatarPostureValues GetZeroPosture(string AvatarID)
+        {
+            List<double> rotation_data = new List<double>();
+            this.j.CreateZeroVector(rotation_data);
+            return new MAvatarPostureValues(AvatarID, rotation_data);
+        }
+
+        public void SetToZero(string AvatarID)
+        {
+
+            MAvatarPostureValues zero = this.GetZeroPosture(AvatarID);
+            this.j.SetAvatarPostureValues(zero);
+        }
+
+
+        public ISVisualizationJoint GetJointByName(string name)
+        {
+            if (this.j.GetMJoint().ID == name)
+            {
+                return this;
+            }
+            ISVisualizationJoint returnValue;
+            foreach (ISVisualizationJoint children in this.children)
+            {
+                returnValue = children.GetJointByName(name);
+                if (returnValue != null)
+                {
+                    return returnValue;
+                }
+            }
+            return null;
+        }
+    }
+
+}
---
+++ Runtime/MMIUnity/UnityColliderFactory.cs
@@ -0,0 +1,188 @@
+﻿// SPDX-License-Identifier: MIT
+// The content of this file has been developed in the context of the MOSIM research project.
+// Original author(s): Felix Gaisbauer
+
+using MMIStandard;
+using System.Globalization;
+using System.Linq;
+using UnityEngine;
+
+namespace MMIUnity
+{
+    /// <summary>
+    /// Class instantiates Unity colliders based on the MCollider class 
+    /// </summary>
+    public static class UnityColliderFactory
+    {
+        /// <summary>
+        /// Creates a Unity collider based on the MCollider and a transform
+        /// </summary>
+        /// <param name="collider"></param>
+        /// <param name="transform"></param>
+        /// <returns></returns>
+        public static Collider CreateCollider(MCollider collider, MTransform transform)
+        {
+            if (collider == null || transform == null)
+                return null;
+
+			Vector3 scale = getScale(collider);
+
+			Collider result = null;
+            switch (collider.Type)
+            {
+                case MColliderType.Box:
+					result = getBoxCollider(collider);
+					break;
+					
+                case MColliderType.Sphere:
+					result = getSphereCollider(collider, scale);
+					break;
+
+                case MColliderType.Capsule:
+					result = getCapsuleCollider(collider);
+					break;
+
+				case MColliderType.Cylinder:
+					result = getCylinderCollider(collider);
+					break;
+
+				case MColliderType.Mesh:
+					result = getMeshCollider(collider);
+					break;
+
+				default:
+					Debug.Log($"Collider type \"{collider.Type}\" is not implemented yet");
+					return null;
+			}
+
+			if (result != null)
+			{
+				var gameobject = result.gameObject;
+				gameobject.transform.position = transform.Position.ToVector3();
+				gameobject.transform.rotation = transform.Rotation.ToQuaternion();
+				gameobject.transform.localScale = scale;
+			}
+
+			return result;
+		}
+
+		private static BoxCollider getBoxCollider(MCollider mCollider)
+		{
+			MBoxColliderProperties boxProps = mCollider.BoxColliderProperties;
+			if (boxProps == null)
+			{
+				Debug.Log("Box collider is null");
+				return null;
+			}
+
+			GameObject box = GameObject.CreatePrimitive(PrimitiveType.Cube);
+			BoxCollider boxCollider = box.GetComponent<BoxCollider>();
+			boxCollider.center = mCollider.PositionOffset.ToVector3();
+			boxCollider.size = boxProps.Size.ToVector3();
+
+			return boxCollider;
+		}
+
+		private static SphereCollider getSphereCollider(MCollider mCollider, Vector3 scale)
+		{
+			MSphereColliderProperties sphereProps = mCollider.SphereColliderProperties;
+			if (sphereProps == null)
+			{
+				Debug.Log("Sphere collider is null");
+				return null;
+			}
+
+			GameObject sphere = GameObject.CreatePrimitive(PrimitiveType.Sphere);
+			SphereCollider sphereCollider = sphere.GetComponent<SphereCollider>();
+			sphereCollider.center = mCollider.PositionOffset.ToVector3();
+			sphereCollider.radius = (float)sphereProps.Radius / Mathf.Max(scale.x, scale.y, scale.z);
+
+			return sphereCollider;
+		}
+
+		private static CapsuleCollider getCapsuleCollider(MCollider mCollider)
+		{
+			MCapsuleColliderProperties capsuleProps = mCollider.CapsuleColliderProperties;
+			if (capsuleProps == null)
+			{
+				Debug.Log("Capsule collider is null");
+				return null;
+			}
+
+			GameObject capsule = GameObject.CreatePrimitive(PrimitiveType.Capsule);
+			CapsuleCollider capsuleCollider = capsule.GetComponent<CapsuleCollider>();
+			capsuleCollider.center = mCollider.PositionOffset.ToVector3();
+			capsuleCollider.radius = (float)capsuleProps.Radius;
+			capsuleCollider.height = (float)capsuleProps.Height;
+			
+			return capsuleCollider;
+		}
+
+		private static CapsuleCollider getCylinderCollider(MCollider mCollider)
+		{
+			MCylinderColliderProperties cylinderProps = mCollider.CylinderColliderProperties;
+
+			if (cylinderProps == null)
+			{
+				Debug.Log("Cylinder collider is null");
+				return null;
+			}
+
+			GameObject cylinder = GameObject.CreatePrimitive(PrimitiveType.Cylinder);
+			CapsuleCollider cylinderCollider = cylinder.GetComponent<CapsuleCollider>();
+			cylinderCollider.center = mCollider.PositionOffset.ToVector3();
+			cylinderCollider.radius = (float)cylinderProps.Radius;
+			cylinderCollider.height = (float)cylinderProps.Height;
+
+			return cylinderCollider;
+		}
+
+		private static MeshCollider getMeshCollider(MCollider mCollider)
+		{
+			MMeshColliderProperties mMeshCollider = mCollider.MeshColliderProperties;
+
+			if (mMeshCollider == null)
+			{
+				Debug.Log("Mesh collider is null");
+				return null;
+			}
+
+			GameObject mesh = new GameObject();
+			MeshFilter meshFilter = mesh.AddComponent<MeshFilter>();
+			MeshRenderer renderer = mesh.AddComponent<MeshRenderer>();
+
+			meshFilter.mesh.SetVertices(mMeshCollider.Vertices.Select(s => s.ToVector3()).ToList());
+			meshFilter.mesh.SetTriangles(mMeshCollider.Triangles, 0);
+			meshFilter.mesh.RecalculateNormals();
+
+			MeshCollider meshCollider = mesh.AddComponent<MeshCollider>();
+			meshCollider.sharedMesh = meshFilter.mesh;
+
+			return meshCollider;
+		}
+
+		//ToDo: remove method and replace with transform.Scale.ToVector3() once scale info is added to MTransform
+		private static Vector3 getScale(MCollider mCollider)
+		{
+			Vector3 result = Vector3.one;
+			if(mCollider.Properties != null && mCollider.Properties.Count > 0)
+			{
+				if (mCollider.Properties.TryGetValue("ScaleX", out string scaleXStr) &&
+					float.TryParse(scaleXStr, NumberStyles.Any,
+						CultureInfo.InvariantCulture, out float scaleX))
+					result.x = scaleX;
+				
+				if (mCollider.Properties.TryGetValue("ScaleY", out string scaleYStr) &&
+					float.TryParse(scaleYStr, NumberStyles.Any,
+						CultureInfo.InvariantCulture, out float scaleY))
+					result.y = scaleY;
+				
+				if (mCollider.Properties.TryGetValue("ScaleZ", out string scaleZStr) &&
+					float.TryParse(scaleZStr, NumberStyles.Any,
+						CultureInfo.InvariantCulture, out float scaleZ))
+					result.z = scaleZ;
+			}
+			return result;
+		}
+    }
+}
